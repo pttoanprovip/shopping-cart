b3c74f92f61c205d7bd7804823e3ad0f40a2913e
Toan <phanthanhtoan248@gmail.com>
2023-10-12 15:20:06 +0700

    Add project files.

---
+++ gio hang/JavaScript.js
@@ -0,0 +1,16 @@
+﻿window.onload = function () {
+    var quantityInput = document.getElementById('quantity');
+    var priceElement = document.querySelector('.price');
+    var totalElement = document.getElementById('total');
+
+    quantityInput.addEventListener('input', function () {
+        calculateTotal();
+    });
+
+    function calculateTotal() {
+        var quantity = parseFloat(quantityInput.value);
+        var price = parseFloat(priceElement.innerHTML);
+        var total = quantity * price;
+        totalElement.innerHTML = total;
+    }
+}
